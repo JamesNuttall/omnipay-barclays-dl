484bb597cba949863d6fed8654667cc957bf677c
James Nuttall <james.nuttall@ghostwhite.net>
2018-10-30 14:48:21 +0000

    tests

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -4,6 +4,7 @@ namespace Omnipay\BarclaysEpdqDl;
 
 use Omnipay\Common\Exception\InvalidRequestException;
 use Omnipay\Tests\GatewayTestCase;
+use Omnipay\BarclaysEpdqDl\Message\PurchaseRequest;
 
 class GatewayTest extends GatewayTestCase
 {
@@ -16,6 +17,28 @@ class GatewayTest extends GatewayTestCase
     {
         parent::setUp();
 
-        $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
+        $this->gateway = new Gateway();
+    }
+
+    public function testPurchase()
+    {       
+        $request = $this->gateway->purchase(
+            [
+                'transactionId' => 'xxxx-test-xxxxx',
+                'amount' => '10.00',
+                'currency' => 'GBP',
+                'card' => [
+                    'number' => '5399999999999999',
+                    'expiryMonth' => '6',
+                    'expiryYear' => '2020',
+                    'cvv' => '123'
+                ]
+            ]
+        );
+
+        $this->assertInstanceOf(PurchaseRequest::class, $request);
+        $this->assertSame('10.00', $request->getAmount());
+        $this->assertSame('GBP', $request->getCurrency());
+        $this->assertSame('xxxx-test-xxxxx', $request->getTransactionId());
     }
 }
